f0c39dcbf3598865a36d81f6ec38a50c4d11f4a4
Ritik-Sharma-Bihari <pythonperson29@gamail.com>
2024-02-18 15:01:16 +0000

    first-commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const elementts = (
+  <div>
+    <h1 className='greeting'>welcome to vignan</h1>
+    <h2 className='greeting'>for BCA cour </h2>
+  </div>
+)
+ReactDOM.render(elementts, document.getElementById('root5'))
